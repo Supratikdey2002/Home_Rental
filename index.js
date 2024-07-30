c07cb62f19c4ff5fa67047dac8d6126b448ca5c8
Supratik Dey <deysupratik07@gmail.com>
2024-07-30 12:10:21 +0530

    commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+const authRoutes = require("./routes/auth.js");
+const listingRoutes = require("./routes/listings.js");
+const BookingRoutes = require("./routes/booking.js");
+const userRoutes = require("./routes/user.js");
+app.use(cors()); //for accessing cross origin resources of different domain
+app.use(express.json()); //for parsing the incoming json formated requests
+app.use(express.static("public"));
+
+/*Routes*/
+app.use("/auth", authRoutes);
+app.use("/properties", listingRoutes);
+app.use("/bookings", BookingRoutes);
+app.use("/users", userRoutes);
+// mongoose setup
+
+const PORT = 3001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((err) => console.log(`${err} did not connect`));
